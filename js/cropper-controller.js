87a802e7d1329a2fbbedefd8e2983ed43dd6c68c
0xChiaChi <king565634@gmail.com>
2021-10-28 14:16:36 +0800

    簡單的radio事件選擇方式添加

--- js/cropper-controller.js
+++ js/cropper-controller.js
@@ -33,48 +33,28 @@ function canvasCrop(source) {
   context.globalCompositeOperation = "destination-in";
   // globalCompositeOperation含12種合成設定，每一種合成設定均將套用到新繪製的圖形上
   //destination-in只保留新、舊圖形重疊的舊圖形區域，其餘皆變為透明。
-  context.beginPath();
-  context.rect(0, 0, width, height);
-  context.fill();
+  // context.beginPath();
+  // context.rect(0, 0, width, height);
+  // context.fill();
   //填充填滿
   return canvas;
 }
 
-button.onclick = function () {
-  if (!croppable) {
-    return;
-  }
-  var image = document.getElementById("imgd");
-  var button = document.getElementById("crop_img");
-  var result = document.getElementById("result");
-  var croppable = false;
-  var cropper = new Cropper(image, {
-    aspectRatio: NaN,
-    //比例
-    viewMode: 0,
-    //裁剪空間可以到外面
-    preview: ".preview",
-    //預覽圖
-    ratotable: true,
-    //可旋轉但我沒做
-    crop(event) {
-      croppable = true;
-      //當正在拉圖的時候表示true(框在上面的時候)
-    },
-  });
-
   button.onclick = function () {
+  	var croppedCanvas;
+  	var roundedCanvas;
+  	var roundedImage;
     if (!croppable) {
       return;
     }
     // Crop
-    var croppedCanvas = cropper.getCroppedCanvas();
+    croppedCanvas = cropper.getCroppedCanvas();
     //返回影象裁剪後，繪製的canvas物件，如果影象未裁剪，則將返回整個影象，繪製的canvas。
     // Round
-    var roundedCanvas = canvasCrop(croppedCanvas);
+    roundedCanvas = canvasCrop(croppedCanvas);
     //繪製canvas。
     // Show
-    var roundedImage = document.createElement("img");
+    roundedImage = document.createElement("img");
     roundedImage.src = roundedCanvas.toDataURL();
     //roundedCanvas轉成圖片+base64模式 (toDataURL)
     result.innerHTML = "";
@@ -82,4 +62,3 @@ button.onclick = function () {
     result.appendChild(roundedImage);
     //生成圖片在result 用生成appendChild子節點的方式
   };
-};
